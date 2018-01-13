8de512e847d287761e74f3844fd7e81e1bcefe62
aaronsato <2asato@gmail.com>
2018-01-12 16:24:25 -0800

    trying to remember

---
+++ first_code.js
@@ -0,0 +1,51 @@
+// let letters = "A";
+// let i = 0;
+//
+// while (i < 20) {
+// 	letters += "A";
+// 	i++;
+// }
+//
+// console.log(letters);
+//
+// for (let i = 0; i <= 999; i++) {
+//   console.log(i);
+// }
+
+// for ( let i = 0; i <=100; i++ ) {
+//   console.log('I need to practice more!!!')
+// }
+
+// for ( let i = 999; i >= 0; i-- ) {
+//   console.log(i);
+// }
+
+// for ( let i = 1; i <= 10; i++ ) {
+//   console.log('The value of i is: ' + i + ' of 10');
+// }
+
+// const StarWars = ["Han", "C3PO", "R2D2", "Luke", "Leia", "Anakin"];
+//
+// for ( let i = 0; i <= StarWars.length; i+=2) {
+//   // console.log(StarWars[i]);
+//   console.log(StarWars[i] + ' ' + i);
+// }
+
+// console.log("Making your way in the world today takes everything you've got.");
+// console.log("Taking a break from all your worries, sure would help a lot.");
+// console.log("Wouldn't you like to get away?");
+// console.log("Sometimes you want to go");
+// console.log("Where everybody knows your name,");
+// console.log("and they're always glad you came.");
+// console.log("You wanna be where you can see,");
+// console.log("our troubles are all the same");
+// console.log("You wanna be where everybody knows");
+// console.log("Your name.");
+
+// for (let i = 0; i <= 100; i++) {
+//   console.log(i);
+// }
+
+// for (let i = 7; i <= 635; i++) {
+//   console.log(i);
+// }
