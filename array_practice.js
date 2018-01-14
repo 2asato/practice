c7ef15784f72f61bc287f7b79fa670b566f858a9
aaronsato <2asato@gmail.com>
2018-01-14 14:49:09 -0800

    mo practice

--- array_practice.js
+++ array_practice.js
@@ -69,13 +69,91 @@
 // console.log(sortedReversed.pop());
 
 
-const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
-                    ["Lucinda", "Jacc", "Neff", "Snoop"],
-                    ["Petunia", "Baked Goods", "Waldo"]];
+// const whereIsWaldo = [["Timmy", "Frank"], "Eggbert",
+//                     ["Lucinda", "Jacc", "Neff", "Snoop"],
+//                     ["Petunia", "Baked Goods", "Waldo"]];
+//
+// console.log(whereIsWaldo[3][2]);
+// console.log(whereIsWaldo[2][1]);
+// whereIsWaldo[3][1] = 'No One'
+// console.log(whereIsWaldo);
+// whereIsWaldo.splice(1, 1);
+// console.log(whereIsWaldo);
 
-console.log(whereIsWaldo[3][2]);
-console.log(whereIsWaldo[2][1]);
-whereIsWaldo[3][1] = 'No One'
-console.log(whereIsWaldo);
-whereIsWaldo.splice(1, 1);
-console.log(whereIsWaldo);
+// let name = 'Joni';
+// console.log(name);
+//
+// if (name === 'Teyo') {
+//   console.log('Hi Teyo!')
+// } else {
+//   console.log('Whats your name?');
+// }
+
+// console.log(name);
+
+// let firstVariable = 'Hello World';
+// firstVariable = 24;
+// let secondVariable = 24;
+// secondVariable = 'Whos on first?';
+// console.log(firstVariable);
+
+// let animal = 'pig';
+//
+// if (animal === 'cow') {
+//   console.log('mooooo');
+// } else {
+//   console.log('Hey! not a cow.');
+// }
+
+// let age = 4;
+//
+// if (age >= 16) {
+//   console.log('Here are the keys');
+// } else {
+//   console.log('Sorry, youre too young.');
+// }
+
+// for (let i = 0; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// for (let i = 10; i <= 4000; i++) {
+//   console.log(i);
+// }
+
+// for (let i = 10; i <= 4000; i+=2) {
+//   console.log(i);
+// }
+
+// for (let i = 1; i <= 100; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i + ' is an even number');
+//   }
+// }
+
+// for (let i = 0; i <= 100; i++) {
+//   if (i % 5 === 0) {
+//     console.log('I found a ' + i + '. High five!');
+//   } else if (i % 3 === 0) {
+//       console.log('I found a ' + i + '. Three is a crowd');
+//   }
+// }
+// let bank_account = 0;
+// for (let i = 1; i <= 10; i++) {
+//   bank_account = i + bank_account;
+// }
+// console.log('$' + bank_account);
+
+// let bank_account = 0;
+// for (let i = 1; i <= 100; i++) {
+//   bank_account = (i * 2) + bank_account;
+// }
+// console.log('$' + bank_account);
+
+// let sum = 0;
+// for (let i = 0; i < 1000; i++) {
+//   if ((i % 3 === 0) || (i % 5 === 0)) {
+//     sum = i + sum;
+//   }
+// }
+// console.log(sum);
