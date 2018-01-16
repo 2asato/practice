dbaa953c158ec01cc9676e1ebef7ef4e2cac5f87
aaronsato <2asato@gmail.com>
2018-01-16 15:03:02 -0800

    part one

--- scope.js
+++ scope.js
@@ -1,7 +1,37 @@
-console.log(Math.sqrt(9));
+// console.log(Math.sqrt(9));
+//
+// console.log(Math.pow(Math.PI, 2));
+//
+// console.log(Math.pow(5, 4));
+//
+// console.log(Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1);
 
-console.log(Math.pow(Math.PI, 2));
+// const printGreeting = (name) => {
+//   return 'Hello there, ' + name + '!';
+// }
+// console.log(printGreeting('Slimer'));
 
-console.log(Math.pow(5, 4));
+// const reverseWordOrder = (str) => {
+//   let result = '';
+//   let wordArray = str.split(' ');
+//   for ( let i = wordArray.length - 1; i >= 0; i--) {
+//     result += wordArray[i] + ' ';
+//   }
+//   return result.trim();
+// }
+// console.log(reverseWordOrder('Ishmael me Call'));
 
-console.log(Math.floor(Math.random() * 6) + 1, Math.floor(Math.random() * 6) + 1);
+const calculate = (num1, num2, operation) => {
+  if (operation === 'add') {
+    return num1 + num2;
+  } else if (operation === 'sub') {
+    return num1 - num2;
+  } else if (operation === 'mult') {
+    return num1 * num2;
+  } else if (operation === 'div') {
+    return num1 / num2;
+  } else if (operation === 'exp') {
+    return Math.pow(num1, num2);
+  }
+}
+console.log(calculate(4, 3, 'exp'));
