733e3889ed2225caeeaf4f15ce1ac90907e32430
aaronsato <2asato@gmail.com>
2018-01-16 09:18:22 -0800

    functions practice

---
+++ functions.js
@@ -0,0 +1,85 @@
+// const printSum = () => {
+//   console.log(10 + 10);
+// }
+// printSum();
+
+// const printTriangle = () => {
+//   console.log('#');
+//   console.log('##');
+//   console.log('###');
+//   console.log('####');
+//   console.log('#####');
+// }
+// printTriangle();
+
+// const printTriangle = () => {
+//   let s = '';
+//   for (let i = 1; i <= 5; i++) {
+//     s += '#';
+//     console.log(s);
+//   }
+// }
+// printTriangle();
+
+// const printParameter = (input) => {
+//   console.log(input);
+// }
+// printParameter('Minnesota Miracle')
+
+// const minusOne =(num)=>{
+//   console.log(num - 1);
+// }
+// minusOne(9999)
+
+// const getLastElement = (arr) => {
+//   console.log(arr[arr.length - 1]);
+// }
+// getLastElement(['NE', 'JAX', 'PHI', 'MIN'])
+
+// const makeSentence = (x, y, z) => {
+//   console.log('Oh boy, do ' + x + ' ' + y + ' ' + z + ' or what?');
+// }
+// makeSentence('I', 'want', 'chimichangas');
+
+// const divideThreeNums = (one, two, three) => {
+//   console.log(three / (two / one));
+// }
+// divideThreeNums(30, 2, 9)
+
+// const calculateArea = (width, length) => {
+//   return width * length;
+// }
+//
+// console.log(calculateArea(10, 10));
+
+// const practice = (x, y, z) => {
+//   let sum = 0;
+//   return sum = x + y + z;
+//   console.log(sum.toString());
+// }
+// console.log(practice(10, 20 , 99));
+
+// const practice = (one, two) => {
+//   if (one === two) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
+// console.log(practice('Joni', 'Joni')
+// );
+
+// const palindrome = (str) => {
+//   let splitString = str.split('');
+//   console.log(splitString);
+//   let reverseString = splitString.reverse();
+//   console.log(reverseString);
+//   let joinString = reverseString.join('');
+//   console.log(joinString);
+//   if (str === joinString) {
+//     console.log(true);
+//   } else {
+//     console.log(false);
+//   }
+// }
+// palindrome('stats');
