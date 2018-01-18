6b726be96e1ff174da1d2e7422cf67c7a51605b2
aaronsato <2asato@gmail.com>
2018-01-17 16:06:26 -0800

    lab practice

---
+++ oop_objects/app.js
@@ -0,0 +1,22 @@
+const garcia = {
+    albums: ['shady grove', 'run for the roses', 'hooteroll', 'Garcia', 'Reflections'],
+    instruments: ['Vocals', 'pedal steel', 'electric guitar', 'acoustic guitar', 'banjo'],
+    alive: false,
+    quote: 'Im shopping around for something to do that no one will like',
+    friends: [{
+      name: 'Bobby weir',
+      hobby: 'guitar'
+    },
+    {
+      name: 'Bob Dylan',
+      hobby: 'writing songs'
+    },
+    {
+      name: 'Allen Ginsberg',
+      hobby: 'writing poems'
+    },
+    {
+      name: 'Phil Lesh',
+      hobby: 'Playing dope bass lines'
+    }]
+}
