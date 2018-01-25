57e6c06fa1b2a711d816c0f97b06f954b54e6109
aaronsato <2asato@gmail.com>
2018-01-25 14:36:20 -0800

    getting started

---
+++ practice.js
@@ -0,0 +1,26 @@
+// const fun = {
+//     asdf: ["afd", "matt", 'sweet']
+// }
+//
+// console.log(fun.asdf[2]);
+
+// const body = {
+//     arm: {
+//         elbow: 'pointy'
+//     }
+// };
+// console.log(body.arm.elbow);
+
+// const person = {
+//     jump(){
+//         console.log('fly!');
+//     }
+// };
+// person.jump();
+
+const myList = [
+  colors = {
+    eyeColor: 'blue',
+  }
+]
+console.log(myList[0].eyeColor);
