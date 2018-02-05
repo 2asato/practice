d105ea86c061a6c349eb093ec3c1edf048a77941
aaronsato <2asato@gmail.com>
2018-02-05 13:54:37 -0800

    3

--- object_answers.js
+++ object_answers.js
@@ -13,6 +13,31 @@
 //   fact: 'I need a job',
 //   statement(){
 //     console.log('My name is ' + myInformation.name + ' , ' + myInformation.fact + ' and in 10 years I will be ' + (myInformation.age + 10) + ' years old.');
+//   },
+//   birthday(){
+//     myInformation.age++;
+//     console.log(myInformation.age);
+//   },
+//   changeName(name){
+//     myInformation.name = name;
 //   }
 // }
 // myInformation.statement();
+// myInformation.birthday();
+// myInformation.changeName('Rumpelstiltskin');
+// console.log(myInformation.name);
+
+// const greeter = {
+//   hello(name){
+//     console.log('Hello there, ' + name);
+//   },
+//   goodbye(name){
+//     console.log('Dueces ' + name);
+//   },
+//   whoAreYou(name){
+//     console.log('Oh right! ' + name + ', how could I forget...!');
+//   }
+// }
+// greeter.hello('Teyo');
+// greeter.goodbye('Teyo');
+// greeter.whoAreYou('Teyo');
