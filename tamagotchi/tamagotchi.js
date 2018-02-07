02de098661ee0991566ceaf729901d3013a52b13
aaronsato <2asato@gmail.com>
2018-02-06 17:01:25 -0800

    created tamagotchi object

---
+++ tamagotchi/tamagotchi.js
@@ -0,0 +1,11 @@
+const tamagotchi1 = {
+  name: 'Noodle',
+  creatureType: 'Corgi',
+  foodInTummy: 10,
+  restedness: 10,
+  health: 10,
+  cry(){
+    console.log('Waaaaa!!!!');
+  }
+}
+tamagotchi1.cry();
