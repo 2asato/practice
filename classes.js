2b6da7d97adc05e7b30f4101423f346ad4571313
aaronsato <2asato@gmail.com>
2018-02-09 13:18:51 -0800

    methods added to class

--- classes.js
+++ classes.js
@@ -1,8 +1,15 @@
 class Person {
-
+        greet(otherPerson){
+          console.log('hi ' + otherPerson + '!');
+        }
+        jump(){
+          console.log('weeee!');
+        }
 };
 const me = new Person();
-const bob = new Person();
+me.greet('Teyo');
+me.jump();
+// const bob = new Person();
 // console.log(me);
 // console.log(bob);
 // console.log(typeof(me));
