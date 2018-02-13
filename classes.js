ce3577eebcb2d22c6ee3a27b9331a48e83309210
aaronsato <2asato@gmail.com>
2018-02-12 23:28:43 -0800

    person pet artist

--- classes.js
+++ classes.js
@@ -1,30 +1,56 @@
-class Person {
-        constructor(name, age, eyes, hair){
-          this.legs = 2;
-          this.arms = 2;
-          this.name = name;
-          this.age = age;
-          this.eyes = eyes;
-          this.hair = hair;
-        }
-        setHair(hairColor){
-          this.hair = hairColor;
-        }
-        greet(otherPerson){
-          console.log('hi ' + otherPerson + '!');
-        }
-        jump(){
-          console.log('weeee!');
-        }
-};
-
-class SuperHero extends Person {
-
-};
+// class Person {
+//         constructor(name, age, eyes, hair){
+//           this.legs = 2;
+//           this.arms = 2;
+//           this.name = name;
+//           this.age = age;
+//           this.eyes = eyes;
+//           this.hair = hair;
+//         }
+//         setHair(hairColor){
+//           this.hair = hairColor;
+//         }
+//         greet(otherPerson){
+//           console.log('hi ' + otherPerson + '!');
+//         }
+//         jump(){
+//           console.log('weeee!');
+//         }
+// };
+//
+// class SuperHero extends Person {
+//
+// };
 
 // const me = new Person('Aaron', 36, 'brown', 'brown');
 // me.setHair('Pink');
 // console.log(me);
 
-const superman = new SuperHero('Clark Kent', 30, 'blue', 'black')
-console.log(superman);
+/// console.log(superman);
+
+class Person {
+  constructor(name, age, eyes, hair) {
+    this.legs = 2;
+    this.arms = 2;
+    this.name = name;
+    this.age = age;
+    this.eyes = eyes;
+    this.hair = hair;
+  }
+};
+
+class Pet {
+  constructor(type, name, age, owner) {
+    this.type = type;
+    this.name = name;
+    this.age = age;
+    this.owner = owner;
+  }
+};
+
+class Artist extends Person {
+
+}
+
+const cas = new Person ('Cas', 15, 'brown', 'brown')
+console.log(cas);
