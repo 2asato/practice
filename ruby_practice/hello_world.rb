157b4e51169a5ef3c11ea21bbe8f90ce94af2f2c
aaronsato <2asato@gmail.com>
2018-02-21 09:42:43 -0800

    hello world

---
+++ ruby_practice/hello_world.rb
@@ -0,0 +1,15 @@
+# p 'Hello world'
+# p 42
+# p true
+# p [1, 2, 3]
+#
+# adjective = 'cruel'
+# p adjective
+#
+# p "Hello #{adjective} world"
+#
+# p adjective.reverse
+# p adjective.upcase
+#
+# adjectives = ['cruel', 'dusty', 'ordinary']
+# p "Hello #{adjectives.sample} world"
