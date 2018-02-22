8f68ccd07c83519cd6c27631b0aa21d8d1e6c01b
aaronsato <2asato@gmail.com>
2018-02-21 23:29:52 -0800

    problem-solving

--- ruby_practice/morning_lab.rb
+++ ruby_practice/morning_lab.rb
@@ -1,4 +1,21 @@
-p 'dope'.upcase
-p 'IM NOT SHOUTING'.downcase
-p 'supercalifragilisticexpialidocious'.length
-p 'i really like programming'.capitalize
+# p 'dope'.upcase
+# p 'IM NOT SHOUTING'.downcase
+# p 'supercalifragilisticexpialidocious'.length
+# p 'i really like programming'.capitalize
+
+# def help_me name
+#   "help me #{name}, help help me #{name}"
+# end
+# p help_me 'Rhonda'
+
+# def sum_numbers a, b, c
+#   a + b + c
+# end
+# p sum_numbers 1, 2, 3
+
+# def convert_time num
+#   hour = num / 60
+#   minutes = num % 60
+#   p "#{hour}:#{minutes}"
+# end
+# convert_time 126
