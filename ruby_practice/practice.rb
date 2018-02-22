ff52a5cacc19d4a2a70bb8fb8e5c73f9ce8f4fab
aaronsato <2asato@gmail.com>
2018-02-22 14:38:28 -0800

    arrays

--- ruby_practice/practice.rb
+++ ruby_practice/practice.rb
@@ -1,4 +1,15 @@
-hi = "Hello World"
-adjective = "Big Bad"
-p "Hello #{adjective} World"
-p hi.upcase
+# hi = "Hello World"
+# adjective = "Big Bad"
+# p "Hello #{adjective} World"
+# p hi.upcase
+
+# nums = [5, 5, 6, 7, 2, 4, 3, 2, 1, 9, 7, 6, 0, 0]
+# p nums.push
+# p nums.pop
+# p nums.length
+# p nums.include? 8
+
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+# p colors.sample
+# p colors.reverse
+p colors.map(&:upcase)
