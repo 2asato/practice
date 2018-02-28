6bb147e9dd5ac617f50a0496b5b5badf87456a30
aaronsato <2asato@gmail.com>
2018-02-28 14:45:06 -0800

    populate world

--- ruby_practice/objects.rb
+++ ruby_practice/objects.rb
@@ -1,21 +1,56 @@
+# class World
+#   def name
+#     @name = "Panthalassa"
+#   end
+#
+#   def name
+#     @name
+#   end
+#
+#   def name=value
+#     @name = value
+#   end
+#
+# end
+#
+# world1 = World.new
+#
+# p world1
+# world1.name = "Booger-town"
+# p world1.name
+# p world1.class
+
 class World
-  def name
+
+  def initialize
     @name = "Panthalassa"
+    @people = []
   end
 
   def name
     @name
   end
 
+  def people
+    @people
+  end
+
   def name=value
     @name = value
+  end
+
+  def populate
+    @people << { name: "Lumpy", age: 20 }
   end
 
 end
 
 world1 = World.new
 
-p world1
-world1.name = "Booger-town"
-p world1.name
-p world1.class
+p world1.people
+
+world1.populate
+world1.populate
+world1.populate
+
+p world1.people
