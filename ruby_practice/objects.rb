12face07b8054e70b1305546774834ca3da695b0
aaronsato <2asato@gmail.com>
2018-02-28 14:37:32 -0800

    update properties

--- ruby_practice/objects.rb
+++ ruby_practice/objects.rb
@@ -1,11 +1,21 @@
 class World
   def name
-    "Panthalassa"
+    @name = "Panthalassa"
   end
+
+  def name
+    @name
+  end
+
+  def name=value
+    @name = value
+  end
+
 end
 
 world1 = World.new
 
 p world1
+world1.name = "Booger-town"
 p world1.name
 p world1.class
