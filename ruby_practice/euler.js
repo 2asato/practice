eaea00588d1c8ae9c04586752a9ec62e9c712b6c
aaronsato <2asato@gmail.com>
2018-02-28 14:33:42 -0800

    making objects

--- ruby_practice/euler.js
+++ ruby_practice/euler.js
@@ -1,7 +1,18 @@
-let sum = 0;
-for (let i = 1; i < 1000; i++) {
-  if (i % 3 == 0 || i % 5 == 0) {
-    sum += i;
-  }
-}
-console.log(sum);
+// let sum = 0;
+// for (let i = 1; i < 1000; i++) {
+//   if (i % 3 == 0 || i % 5 == 0) {
+//     sum += i;
+//   }
+// }
+// console.log(sum);
+
+// const smallestMultiple = (num) => {
+//   for (let i = 1; i < 21; i++) {
+//     if (num % i == 0) {
+//       console.log('works');
+//     } else {
+//       console.log('nope');
+//     }
+//   }
+// }
+// console.log(smallestMultiple(2520));
---
+++ ruby_practice/objects.rb
@@ -0,0 +1,11 @@
+class World
+  def name
+    "Panthalassa"
+  end
+end
+
+world1 = World.new
+
+p world1
+p world1.name
+p world1.class
