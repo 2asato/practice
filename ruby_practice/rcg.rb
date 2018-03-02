3b41b09d6b8c6bcf457aae5101f3b940b631f631
aaronsato <2asato@gmail.com>
2018-03-01 21:44:34 -0800

    simple word guess

--- ruby_practice/rcg.rb
+++ ruby_practice/rcg.rb
@@ -1,3 +1,21 @@
 # puts "Write something: "
 # input = gets
 # puts "You wrote: #{input}"
+
+dictionary = ["potato", "honey", "tea", "rice", "quinoa"]
+word = dictionary.sample
+# p word
+puts "Guess which word I secretly chose!"
+puts "The word has #{word.length} letters."
+
+input = gets.chomp
+
+if input == word
+  puts "Yep. You got it.  The word was #{word}."
+else
+  puts "WRONG"
+  puts "(t)ry again or (q)uit?"
+  again = gets.chomp.downcase
+  if again == "q" then puts "ok"
+  end
+end
