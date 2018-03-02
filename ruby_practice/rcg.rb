7d0bca6e227f5d263d9f29ba7e5a43d9d1b3ebb1
aaronsato <2asato@gmail.com>
2018-03-01 22:01:33 -0800

    create muppet

--- ruby_practice/rcg.rb
+++ ruby_practice/rcg.rb
@@ -1,21 +1,46 @@
-# puts "Write something: "
-# input = gets
-# puts "You wrote: #{input}"
+# # puts "Write something: "
+# # input = gets
+# # puts "You wrote: #{input}"
+#
+# dictionary = ["potato", "honey", "tea", "rice", "quinoa"]
+# word = dictionary.sample
+# # p word
+# puts "Guess which word I secretly chose!"
+# puts "The word has #{word.length} letters."
+#
+# input = gets.chomp
+#
+# if input == word
+#   puts "Yep. You got it.  The word was #{word}."
+# else
+#   puts "WRONG"
+#   puts "(t)ry again or (q)uit?"
+#   again = gets.chomp.downcase
+#   if again == "q" then puts "ok"
+#   end
+# end
 
-dictionary = ["potato", "honey", "tea", "rice", "quinoa"]
-word = dictionary.sample
-# p word
-puts "Guess which word I secretly chose!"
-puts "The word has #{word.length} letters."
+class Muppet
 
-input = gets.chomp
-
-if input == word
-  puts "Yep. You got it.  The word was #{word}."
-else
-  puts "WRONG"
-  puts "(t)ry again or (q)uit?"
-  again = gets.chomp.downcase
-  if again == "q" then puts "ok"
+  Color = ["red", "blue", "green", "orange"]
+  def initialize name
+    p "This muppet is called #{name}!"
+    @name = name
+    @color = Color.sample
   end
+
+  def name
+    @name
+  end
+
+  def color
+    @color
+  end
+
+
 end
+
+muppet1 = Muppet.new "Fonzy"
+p muppet1
+p muppet1.name
+p muppet1.color
