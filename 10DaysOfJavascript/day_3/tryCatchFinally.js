8a6a3350c935d956b775e1f94e344c349b20ce9b
aaronsato <2asato@gmail.com>
2018-04-05 12:34:27 -0700

    added try and catch

--- 10DaysOfJavascript/day_3/tryCatchFinally.js
+++ 10DaysOfJavascript/day_3/tryCatchFinally.js
@@ -1,14 +1,20 @@
 https://www.hackerrank.com/challenges/js10-try-catch-and-finally/problem
 
 function reverseString(s) {
-  let splitS = s.split('');
-  // converts string into array of individual strings
-  console.log(splitS);
-  let reverseS = splitS.reverse();
-  // reverses splitS
-  console.log(reverseS);
-  let newS = reverseS.join('');
-  // joins array of strings back together into one string
-  console.log(newS);
+  try {
+    let splitS = s.split('');
+    // converts string into array of individual strings
+    console.log(splitS);
+    let reverseS = splitS.reverse();
+    // reverses splitS
+    console.log(reverseS);
+    let newS = reverseS.join('');
+    // joins array of strings back together into one string
+    console.log(newS);
+  }
+  catch (error) {
+    console.log(error.message);
+    console.log(s);
+  }
 }
-reverseString('1234')
+reverseString(1234)
