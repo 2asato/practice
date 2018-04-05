e024087e18482468bac4b03dd36921f7b0e254e0
aaronsato <2asato@gmail.com>
2018-04-05 12:26:28 -0700

    comments

--- 10DaysOfJavascript/day_3/tryCatchFinally.js
+++ 10DaysOfJavascript/day_3/tryCatchFinally.js
@@ -2,10 +2,13 @@ https://www.hackerrank.com/challenges/js10-try-catch-and-finally/problem
 
 function reverseString(s) {
   let splitS = s.split('');
+  // converts string into array of individual strings
   console.log(splitS);
   let reverseS = splitS.reverse();
+  // reverses splitS
   console.log(reverseS);
   let newS = reverseS.join('');
+  // joins array of strings back together into one string
   console.log(newS);
 }
 reverseString('1234')
