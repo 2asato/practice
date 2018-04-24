d5f0118c709124bdfcb9e6ad4992258f5bc53b40
aaronsato <2asato@gmail.com>
2018-04-23 23:42:22 -0700

    comments

--- 10DaysOfJavascript/day_8/createAButton.js
+++ 10DaysOfJavascript/day_8/createAButton.js
@@ -1,11 +1,17 @@
 // https://www.hackerrank.com/challenges/js10-create-a-button
 
 let clickMeButton = document.createElement('button');
+// creates button
 clickMeButton.id = ('btn');
+// creates buttons id
 clickMeButton.innerHTML = 0;
+// sets buttons inner text
 document.body.appendChild(clickMeButton);
+// appends button to html
 
 clickMeButton.addEventListener("click",function() {
   let current_value = this.innerHTML;
+  // sets buttons text to current_value
   this.innerHTML = parseInt(current_value)+1;
+  // current_value text increases by 1 every click
 });
