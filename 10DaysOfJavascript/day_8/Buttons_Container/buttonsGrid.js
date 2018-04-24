73b1968406135d6f347a2d80928964f44a7c2066
aaronsato <2asato@gmail.com>
2018-04-24 14:09:49 -0700

    comments

--- 10DaysOfJavascript/day_8/Buttons_Container/buttonsGrid.js
+++ 10DaysOfJavascript/day_8/Buttons_Container/buttonsGrid.js
@@ -1,8 +1,12 @@
 let b = document.getElementById('btn5');
+// sets b to btn5
 let btnArray = [1, 2, 3, 6, 9, 8, 7, 4];
+// array for order of buttons
 
 b.onclick = function(){
+  // when b is clicked
     btnArray.unshift(btnArray.pop());
+    // the last btn in array is popped out and placed at beginning of array
     document.getElementById('btn1').innerHTML = btnArray[0];
     document.getElementById('btn2').innerHTML = btnArray[1];
     document.getElementById('btn3').innerHTML = btnArray[2];
