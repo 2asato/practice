6d9c14c885af35680bb51776ecdddf4a052e2d19
aaronsato <2asato@gmail.com>
2018-05-01 09:58:41 -0700

    1s can be entered into res box

--- 10DaysOfJavascript/day_9/binaryCalculator.js
+++ 10DaysOfJavascript/day_9/binaryCalculator.js
@@ -1,4 +1,9 @@
-// inserts 0s into res box
+// inserts 0s into 'res' box
 document.getElementById('btn0').addEventListener('click', function() {
   document.getElementById('res').insertAdjacentHTML('beforeend', '0');
+});
+
+// inserts 1s into 'res' box
+document.getElementById('btn1').addEventListener('click', function() {
+  document.getElementById('res').insertAdjacentHTML('beforeend', '1')
 });
