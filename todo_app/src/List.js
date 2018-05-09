4e8aee2f4b8e804a86cb6b42db5fec6286549628
aaronsato <2asato@gmail.com>
2018-05-09 14:25:10 -0700

    can clear and strike through todos

--- todo_app/src/List.js
+++ todo_app/src/List.js
@@ -1,13 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const List = (props) => {
-  // console.log('these are the props: ', props);
-  const listItems = props.todos.map(item => <li>{ item }</li>);
-  return (
-    <ul>
-      { listItems }
-    </ul>
-  )
+class List extends Component {
+  constructor(props) {
+  super()
+  console.log('List props: ', props);
+  this.state = {
+    todos: props.todos
+  }
 }
+
+  handleClick() {
+    console.log('clicked');
+    this.setState({
+      todos: []
+    })
+  }
+
+  updateUI(event) {
+    console.log(event.target.className='strikeout');
+  }
+
+  render() {
+    const listItems = this.state.todos.map(item =>
+      <li onClick={ this.updateUI.bind(this) }>{ item }</li>);
+    return (
+      <div>
+        <button onClick={ this.handleClick.bind(this) }>Clear</button>
+        <ul>
+          { listItems }
+        </ul>
+      </div>
+    )
+  }
+}
+
+// const List = (props) => {
+//   // console.log('these are the props: ', props);
+//   const listItems = props.todos.map(item => <li>{ item }</li>);
+//   return (
+//     <ul>
+//       { listItems }
+//     </ul>
+//   )
+// }
 
 export default List;
