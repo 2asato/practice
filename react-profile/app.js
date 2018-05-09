072cf7cb179559cbdacef6a08d80efcc52afc27d
aaronsato <2asato@gmail.com>
2018-05-09 10:31:20 -0700

    filled in App elements with components

--- react-profile/app.js
+++ react-profile/app.js
@@ -6,17 +6,38 @@ console.log('hello world');
 const App = () => {
   return (
     <div>
-      <header>
-        <h1>Website</h1>
-      </header>
-      <main>
-        <h2>I am Bib Fortuna</h2>
-        <img src="https://i.pinimg.com/236x/86/9b/6d/869b6d02226dfdd4b0ddc49086417f13--jabbas-palace-bibs.jpg"/>
-      </main>
-      <footer>
-        <small>Die wanna wanga</small>
-      </footer>
+      <Header />
+      <AboutMe />
+      <Footer />
     </div>
+  )
+}
+
+// AboutMe component
+const AboutMe = () => {
+  return (
+    <main>
+      <h2>I am Bib Fortuna</h2>
+      <img src='https://i.pinimg.com/236x/86/9b/6d/869b6d02226dfdd4b0ddc49086417f13--jabbas-palace-bibs.jpg'/>
+    </main>
+  )
+}
+
+// Header component
+const Header = () => {
+  return (
+    <header>
+      <h1>Website</h1>
+    </header>
+  )
+}
+
+// Footer component
+const Footer = () => {
+  return (
+    <footer>
+      <small>Die wanna wanga</small>
+    </footer>
   )
 }
 
