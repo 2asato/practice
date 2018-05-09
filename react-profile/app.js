50d300f48c50716feefda0d91b026efec072eeab
aaronsato <2asato@gmail.com>
2018-05-09 10:20:32 -0700

    added App component function with header, main, footer

--- react-profile/app.js
+++ react-profile/app.js
@@ -1,6 +1,22 @@
 console.log('hello world');
 
+// functional component
+// component to pass to ReactDOM.render()
+// container component for our site
+const App = () => {
+  return (
+    <div>
+      <header>...</header>
+      <main>...</main>
+      <footer>...</footer>
+    </div>
+  )
+}
+
+// takes 2 arguments
 ReactDOM.render(
-  <div>Hello World</div>,
+  // 1 - element to render, written in JSX
+  <App />,
+  // 2 - location to render
   document.querySelector('#root')
 );
