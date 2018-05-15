362bfb478f978f9b65e37f3e709d548a4c5e113c
aaronsato <2asato@gmail.com>
2018-05-15 13:23:18 -0700

    ajax request to question api successful

--- react_practice/questions_answers_app/src/App.js
+++ react_practice/questions_answers_app/src/App.js
@@ -4,7 +4,10 @@ import './App.css';
 class App extends Component {
 
   componentDidMount() {
-    console.log('component has mounted');
+    fetch('http://jservice.io/api/random?count=10')
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(err => console.log(err))
   }
 
   render() {
