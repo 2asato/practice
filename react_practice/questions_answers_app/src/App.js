aad0cd50abcf0d45e37c3548140287ea9c162ae7
aaronsato <2asato@gmail.com>
2018-05-15 13:19:05 -0700

    componentDidMount() to app.js

--- react_practice/questions_answers_app/src/App.js
+++ react_practice/questions_answers_app/src/App.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    console.log('component has mounted');
+  }
+
   render() {
     return (
       <div></div>
+    );
   }
 }
 
