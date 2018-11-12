7ef3b8f7ae65ab7705a488840c08cdcaf893202c
aaronsato <2asato@gmail.com>
2018-11-12 09:42:17 -0800

    cleanup

--- 10DaysOfJavascript/day_9/binaryCalculator.js
+++ 10DaysOfJavascript/day_9/binaryCalculator.js
@@ -13,3 +13,15 @@ document.getElementById('btnClr').onclick = function()
 {
     (document.getElementById('res').innerHTML) = "";
 };
+
+function action(e) {
+  let btn = e.target
+  /* Get the clicked element's innerHTML */
+  document.getElementById('res').insertAdjacentHTML('beforeend', btn.innerHTML);
+}
+
+/* Set each button to call action(e) when clicked */
+document.getElementById('btnSum').onclick = action;
+document.getElementById('btnSub').onclick = action;
+document.getElementById('btnMul').onclick = action;
+document.getElementById('btnDiv').onclick = action;
